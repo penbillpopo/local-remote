eea63e61b7ddf3ccaee9594adc4e85efb60f21e3
penbillpopo <penbillpopo@gmail.com>
2026-05-25 18:52:30 +0800

    配置部署設定

---
+++ vite.config.playground.ts
@@ -0,0 +1,17 @@
+import { resolve } from 'node:path'
+import vue from '@vitejs/plugin-vue'
+import { defineConfig } from 'vite'
+
+export default defineConfig({
+  base: '/local-remote/',
+  plugins: [vue()],
+  resolve: {
+    alias: {
+      '@': resolve(__dirname, 'src'),
+    },
+  },
+  build: {
+    outDir: 'dist-playground',
+    emptyOutDir: true,
+  },
+})
